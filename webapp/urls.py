bc1c578c31fe57a73a4d1fc85fbba648e9010dd0
yedige87 <yedabdygapparov@gmail.com>
2023-04-14 03:04:47 +0600

    Добавлена программа "get_token_view" для получения кода CSRFToken

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import json_add_view, json_subtract_view, json_multiply_view, json_divide_view
+from webapp.views import json_add_view, json_subtract_view, json_multiply_view, json_divide_view, get_token_view
 
 urlpatterns = [
+    path('get_token', get_token_view),
     path('add', json_add_view, name='add_view'),
     path('subtract', json_subtract_view, name='subtract_view'),
     path('multiply', json_multiply_view, name='multiply_view'),
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,15 @@
 # Create your views here.
 import json
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
 
 
 def json_add_view(request, *args, **kwargs):

59decbf28214675da4dcea73909cf2486a71758d
yedige87 <yedabdygapparov@gmail.com>
2023-04-14 02:52:31 +0600

    Создана точка входа "add" - сложение двух чисел и соответствующее представление

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
+from webapp.views import json_add_view
 
 urlpatterns = [
-   
+    path('add', json_add_view, name='add_view'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+import json
+
+from django.http import JsonResponse
+
+
+def json_add_view(request, *args, **kwargs):
+    if request.body:
+        vals = json.loads(request.body)
+        a = vals['A']
+        b = vals['B']
+        return JsonResponse({"answer": a+b})
+    return JsonResponse('error: 400 BadRequest')
+

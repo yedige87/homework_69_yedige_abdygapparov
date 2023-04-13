8ca44f65e63a98c682383cce8f7f12cf33f14185
yedige87 <yedabdygapparov@gmail.com>
2023-04-14 02:57:29 +0600

    Создана точка входа "multiply" - умножение двух чисел и соответствующее представление

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from webapp.views import json_add_view, json_subtract_view
+from webapp.views import json_add_view, json_subtract_view, json_multiply_view
 
 urlpatterns = [
     path('add', json_add_view, name='add_view'),
     path('subtract', json_subtract_view, name='subtract_view'),
+    path('multiply', json_multiply_view, name='multiply_view'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -21,3 +21,12 @@ def json_subtract_view(request, *args, **kwargs):
         return JsonResponse({"answer": a - b})
     return JsonResponse('error: 400 BadRequest')
 
+
+def json_multiply_view(request, *args, **kwargs):
+    if request.body:
+        vals = json.loads(request.body)
+        a = vals['A']
+        b = vals['B']
+        return JsonResponse({"answer": a * b})
+    return JsonResponse('error: 400 BadRequest')
+

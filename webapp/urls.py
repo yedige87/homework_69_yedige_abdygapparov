c9b83ea7b68693b6635fd2fbfd381accbd926bcb
yedige87 <yedabdygapparov@gmail.com>
2023-04-14 03:10:45 +0600

    В проект добавлен клиент с соответствующими элементами управления для каждой точки входа. Все API-запросы выполняются в одной функции, CSRFToken определяется автоматически по document.cookie

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from webapp.views import json_add_view, json_subtract_view, json_multiply_view, json_divide_view, get_token_view
+from webapp.views import json_add_view, json_subtract_view, json_multiply_view, json_divide_view, get_token_view, \
+    index_view
 
 urlpatterns = [
+    path('', index_view, name='index'),
     path('get_token', get_token_view),
     path('add', json_add_view, name='add_view'),
     path('subtract', json_subtract_view, name='subtract_view'),
--- webapp/views.py
+++ webapp/views.py
@@ -2,7 +2,12 @@
 import json
 
 from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+def index_view(request):
+    return render(request, 'index.html')
 
 
 @ensure_csrf_cookie

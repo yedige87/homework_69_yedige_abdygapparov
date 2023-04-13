b282fbdeb17697d201f21597625e5ba78219df84
yedige87 <yedabdygapparov@gmail.com>
2023-04-14 02:55:17 +0600

    Создана точка входа "subtract" - вычитание двух чисел и соответствующее представление

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import json_add_view
+from webapp.views import json_add_view, json_subtract_view
 
 urlpatterns = [
     path('add', json_add_view, name='add_view'),
+    path('subtract', json_subtract_view, name='subtract_view'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -12,3 +12,12 @@ def json_add_view(request, *args, **kwargs):
         return JsonResponse({"answer": a+b})
     return JsonResponse('error: 400 BadRequest')
 
+
+def json_subtract_view(request, *args, **kwargs):
+    if request.body:
+        vals = json.loads(request.body)
+        a = vals['A']
+        b = vals['B']
+        return JsonResponse({"answer": a - b})
+    return JsonResponse('error: 400 BadRequest')
+
